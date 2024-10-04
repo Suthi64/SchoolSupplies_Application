8e3534e9bb3469a0a760d22a1a41a1f031737b42
root <root@LAPTOP-PL31URTM>
2024-10-04 17:50:10 -0400

    Completed set 6 except for level in startOrder()

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet6Controller.java
@@ -1,30 +1,201 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.model.Student;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.model.Parent;
+import ca.mcgill.ecse.coolsupplies.model.BundleItem.PurchaseLevel;
 
 public class CoolSuppliesFeatureSet6Controller {
 
+  private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
+
   public static String addStudentToParent(String studentName, String parentEmail) {
-    throw new UnsupportedOperationException("Not implemented yet.");
-  }
+
+    var error = "";
+    Parent parent = null;
+    for (Parent parentX : coolSupplies.getParents()) {
+      if (parentX.getEmail().equals(parentEmail)) {
+        parent = parentX;
+        break;
+      }
+    }
+
+    if (parent == null) {
+      error = "The parent does not exist.";
+      return error.trim();
+    }
+
+    Student student = null;
+    for (Student studentX : coolSupplies.getStudents()) {
+      if (studentX.getName().equals(studentName)) {
+        student = studentX;
+        break;
+      }
+    }
+
+    if (student == null) {
+      error = "The student does not exist.";
+      return error.trim();
+    }
+
+    try {
+      parent.addStudent(student);
+    } 
+    catch (RuntimeException e) {
+      error = e.getMessage();
+    }
+    return error;
+
+    }
+  
 
   public static String deleteStudentFromParent(String studentName, String parentEmail) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    var error = "";
+    Parent parent = null;
+    for (Parent parentX : coolSupplies.getParents()) {
+      if (parentX.getEmail().equals(parentEmail)) {
+        parent = parentX;
+        break;
+      }
+    }
+
+    if (parent == null) {
+      error = "The parent does not exist.";
+      return error.trim();
+    }
+
+    Student student = null;
+    for (Student studentX : coolSupplies.getStudents()) {
+      if (studentX.getName().equals(studentName)) {
+        student = studentX;
+        break;
+      }
+    }
+
+    if (student == null) {
+      error = "The student does not exist.";
+      return error.trim();
+    }
+
+    try {
+      parent.removeStudent(student);
+    } 
+    catch (RuntimeException e) {
+      error = e.getMessage();
+    }
+    return error;
+
   }
 
   public static TOStudent getStudentOfParent(String studentName, String parentEmail) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    Parent parent = null;
+    for (Parent parentX : coolSupplies.getParents()) {
+      if (parentX.getEmail().equals(parentEmail)) {
+        parent = parentX;
+        break
+      }
+    }
+
+    if (parent == null) {
+      return null;
+    }
+
+    Student student = null;
+    TOStudent students = new TOStudent();
+    for (Student studentX : parent.getStudents()) {
+      if (studentX.getName().equals(studentName)) {
+        student = studentX;
+        break
+      }
+    }
+
+    if (student == null) {
+      return null;
+    }
+
+    return new TOStudent(student.getName(), student.getGrade().getLevel());
   }
 
   // returns all students of a parent
   public static List<TOStudent> getStudentsOfParent(String parentEmail) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    Parent parent = null;
+    for (Parent parentX : coolSupplies.getParents()) {
+      if (parentX.getEmail().equals(parentEmail)) {
+        parent = parentX;
+        break
+      }
+    }
+
+    if (parent == null) {
+      return null;
+    }
+
+    List<TOStudent> students = new ArrayList<TOStudent>();
+    for (Student student : parent.getStudents()) {
+      students.add(new TOStudent(student.getName(), student.getGrade().getLevel()));
+    }
+    return students;
   }
 
-  public static String startOrder(int number, Date date, String level, String parentEmail,
-      String studentName) {
-    throw new UnsupportedOperationException("Not implemented yet.");
-  }
+  public static String startOrder(int number, Date date, String level, String parentEmail, String studentName) {
+        
+    var error = "";
+    if (number <= 0) {
+      error = "The order should have items.";
+      return error.trim();
+    }
 
+    Parent parent = null;
+    for (Parent parentX : coolSupplies.getParents()) {
+      if (parentX.getEmail().equals(parentEmail)) {
+        parent = parentX;
+        break;
+      }
+    }
+
+    if (parent == null) {
+      error = "The parent does not exist.";
+      return error.trim();
+    }
+
+    Student student = null;
+    for (Student studentX : coolSupplies.getStudents()) {
+      if (studentX.getName().equals(studentName)) {
+        student = studentX;
+        break;
+      }
+    }
+
+    if (student == null) {
+      error = "The student does not exist.";
+      return error.trim();
+    }
+  
+    PurchaseLevel purchaseLevel = null;
+    for (PurchaseLevel purchaseLevelX : coolSupplies.getStudents()) {
+      if (purchaseLevelX.equals(level)) { // to change since for now i did not found how to compare them
+        purchaseLevel = purchaseLevelX;
+        break;
+      }
+    }
+
+    if (purchaseLevel == null) {
+      error = "The purchase level does not exist.";
+      return error.trim();
+    }
+
+  try {
+    Order order = new Order(number, date, level, parent, student, coolSupplies);
+  }
+  catch (RuntimeException e) {
+    error = e.getMessage();
+  }
+    return error;
 }
