513139f567c7690e41f21fa8fc88584374de31e0
mouselzein <moustapha.elzein@mail.mcgill.ca>
2024-10-18 20:07:43 -0400

    Grade implementation not tested

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
@@ -1,28 +1,115 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.model.Grade;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoolSuppliesFeatureSet7Controller {
 
+	private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
+	
+	
   public static String addGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    if (level == null) {
+    	return "The grade level must not be null.";
+    }
+    if (level.isEmpty()) {
+    	return "The grade level must not be empty.";
+    }
+    
+	try {
+		    coolSupplies.addGrade(level);
+	} catch (RuntimeException e) {
+		return e.getMessage(); //Error during run time
+		}
+	return ""; //The grade was successfully added
   }
 
   public static String updateGrade(String level, String newLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    if (level == null) {
+    	return "The grade level must not be null.";
+    }
+    if (level.isEmpty()) {
+    	return "The grade level must not be empty.";
+    }
+    
+    Grade grade = Grade.getWithLevel(level); //Get the grade object to be updated
+    if (grade == null) {
+    	return "The grade does not exist.";
+    }
+    
+    
+    if (newLevel == null) {
+    	return "The new grade level must not be null.";
+    }
+    if (newLevel.isEmpty()) {
+    	return "The new grade level must not be empty.";
+    }
+    
+    Grade newGrade = Grade.getWithLevel(newLevel); //Create a grade object to replace the "old" grade
+    if (newGrade == null){
+    	return "The new grade does not exist.";
+    }
+    
+	try {
+    	grade.delete();
+    	coolSupplies.addGrade(newGrade);
+    } catch(RuntimeException e) {
+    	return e.getMessage(); //Error during run time
+    }
+    
+    
+    return ""; //The grade was successfully updated 
   }
 
   public static String deleteGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    if (level == null) {
+    	return "The grade level must not be null.";
+    }
+    if (level.isEmpty()) {
+    	return "The grade level must not be empty.";
+    }
+    
+	Grade grade = Grade.getWithLevel(level); //Get grade to be deleted
+	if (grade == null) {
+    	return "The grade does not exist.";
+    }
+   
+	try {
+		grade.delete();
+	}
+	catch (RuntimeException e) {
+		return e.getMessage(); //Error during run time
+	}  
+    
+    return ""; //The grade was successfully deleted 
   }
 
   public static TOGrade getGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+	  Grade grade = Grade.getWithLevel(level);
+	  if (grade == null) { //Check if the grade corresponding to the level exists
+	    return null;
+	  }
+	  
+	  return new TOGrade(level); //Return the newly created TOGrade
+	  
   }
 
   // returns all grades
   public static List<TOGrade> getGrades() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+	  List<TOGrade> grades = new ArrayList<TOGrade>(); 
+	  
+	  for (Grade grade: coolSupplies.getGrades()) { //Extract all the grades from the system and add them in grades
+		  String level = grade.getLevel();
+		  grades.add(new TOGrade(level));
+	  }
+	  return grades; 
   }
 
 }
