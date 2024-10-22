cf446d390f1eacf6146a31a0752365e381d1613b
sanadi3 <156261448+sanadi3@users.noreply.github.com>
2024-10-22 19:12:08 -0400

    javadocs added

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
@@ -9,14 +9,26 @@ import ca.mcgill.ecse.coolsupplies.model.SchoolAdmin;
 import java.util.List;
 import java.util.ArrayList;
 
+
+/** 
+ * This is the main controller implementation for FeatureSet1 of the CoolSuppliesApplication
+ * It contains methods relating to manipulating Admin and Parent objects
+ * 
+ * @author Sanad Abu Baker
+*/
 public class CoolSuppliesFeatureSet1Controller {
 
+    /*
+     * Defining admin object with set username and accessible list of parents
+     */
     private static final CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
     private static SchoolAdmin admin = new SchoolAdmin("admin@cool.ca", "admin", coolSupplies); 
     // Predefined admin object
     private static List<Parent> parents = new ArrayList<>(); // List of parents
 
-    // Helper methods
+    /*
+     * Helper methods for identifying Parent from list of parents, and to help in validating emails.
+     */
     private static Parent findParentByEmail(String email) {
         for (Parent parent : CoolSuppliesApplication.getCoolSupplies().getParents()) {
             if (parent.getEmail().equals(email)) {
@@ -44,6 +56,13 @@ public class CoolSuppliesFeatureSet1Controller {
         return false;
     }
 
+    /*
+     * Updating the admin's password.
+     * 
+     * @param password    The new password entered to be updated. If the password is less than the minimum length and doesn't contain the appropriate characters an error message is returned.
+     * 
+     * @return      A string indicating if the operation was successful or not.
+     */
     public static String updateAdmin(String password) {
         if (password == null || password.equals("")) {
             return "Password must not be empty or null.";
@@ -63,12 +82,22 @@ public class CoolSuppliesFeatureSet1Controller {
         // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public static String addParent(String email, String password, String name, String phoneNumber) {
+    /*
+     * Method to add a new Parent with fields of email, password, name, and phone number. This method also adds the created parent to our initialized array of parents.
+     * 
+     * @param email         This email must be checked against various character constraints and if it's already taken.
+     * @param password      The password must not be empty or null.
+     * @param name          The name must not be empty or null.
+     * @param phoneNumber   The phone number must be 7 digits long and cannot contain trailing zeroes.
+     * 
+     * @return              A string indicating if the operation was successful or not.
+     */
+    public static String addParent(String email, String password, String name, int phoneNumber) {
         if (email.contains(" ")) {
             return "Spaces are not allowed in emails!";
         }
 
-        if (email.equals("admin@cool.ca")) {
+        if (email.equals("admin@cool.ca")) { //admin's email cannot be taken.
             return "Email unavailable.";
         }
 
@@ -80,7 +109,7 @@ public class CoolSuppliesFeatureSet1Controller {
             return "Invalid email format.";
         }
 
-        for (Parent parent : parents) {
+        for (Parent parent : parents) { //checking if entered email is taken.
             if (parent.getEmail().equals(email)) {
                 return "A parent with this email already exists.";
             }
@@ -90,15 +119,14 @@ public class CoolSuppliesFeatureSet1Controller {
             return "Please enter a password.";
         }
 
-        int phoneNo = Integer.parseInt(phoneNumber);
-        Parent newParent = new Parent(email, password, name, phoneNo, coolSupplies);
+        Parent newParent = new Parent(email, password, name, phoneNumber, coolSupplies);
 
         if (name != null && !name.equals("")) {
             newParent.setName(name);
         }
 
-        if (phoneNumber != null && !phoneNumber.equals("") && phoneNumber.length() > 6 && !phoneNumber.equals("10000000")) {
-            newParent.setPhoneNumber(phoneNo);
+        if (phoneNumber < 1000000 || phoneNumber > 9999999) {
+            return "Phone number must be 7 digits and cannot have trailing zeroes.";
         }
 
         parents.add(newParent);
@@ -106,30 +134,52 @@ public class CoolSuppliesFeatureSet1Controller {
         // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public static String updateParent(String email, String newPassword, String newName, String newPhoneNumber) {
+    /*
+     * This method is used to update the various fields of the Parent object such as the email, password, name, and phone number.
+     * 
+     * @param email           The email/username of the parent. The email cannot be changed and is thus used to perform checks such as making sure the entered email corresponds to an existing parent.
+     * @param newPassword     The new password of the parent. This password cannot be null or empty and we thus perform a check for that.
+     * @param newName         The new name of the parent. The name cannot be null or empty and we thus perform a check for that.
+     * @param newPhoneNumber  The new phone number of the parent. The new phone number must be exactly 7 digits and cannot have trailing zeroes (1000000).
+     * 
+     * @return                A string indicating if the operation was successful.
+     */
+    public static String updateParent(String email, String newPassword, String newName, int newPhoneNumber) {
+        
+        // Find parent by email. If parent not found, function returns an error message.
         Parent parent = findParentByEmail(email);
         if (parent == null) {
             return "Parent not found.";
         }
 
+        // Update the password if it is not empty.
         if (newPassword != null && !newPassword.equals("")) {
             parent.setPassword(newPassword);
         }
+
+        // Update the name if it is not empty.
         if (newName != null && !newName.equals("")) {
             parent.setName(newName);
         }
-
-        int newPhoneNo = Integer.parseInt(newPhoneNumber);
-
-        if (newPhoneNumber != null) {
-            if (newPhoneNo > 999999 && newPhoneNo < 10000000) {
+ 
+        // Update the phone number if it's a valid 7 digit number.
+        if (newPhoneNumber > 999999 && newPhoneNo < 10000000) {
                 parent.setPhoneNumber(newPhoneNo);
             }
-        }
+        
+        // Success message
         return "Parent updated successfully.";
         // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
+
+    /*
+     * This method is used to delete a parent from the system.
+     * 
+     * @param email     We use this email to find the parent object in our list of parents
+     * 
+     * @return      A string indicating if the operation was successful.
+     */
     public static String deleteParent(String email) {
         Parent parent = findParentByEmail(email);
         if (parent == null) {
@@ -141,6 +191,13 @@ public class CoolSuppliesFeatureSet1Controller {
         // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
+    /*
+     * Retrieves the details of a Parent based on their email.
+     * 
+     * @param email     The email of the parent used to identify them.
+     * 
+     * @return          TOParent transfer object containng their information.
+     */
     public static TOParent getParent(String email) {
         Parent parent = findParentByEmail(email);
         if (parent == null) {
@@ -150,7 +207,13 @@ public class CoolSuppliesFeatureSet1Controller {
         // throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    // Returns all parents
+    /* 
+     * Retrieves a list of all parents in the system.
+     * 
+     * @param none
+     * 
+     * @return    A list of TOParent transfer objects.
+    */
     public static List<TOParent> getParents() {
         List<TOParent> toParents = new ArrayList<>();
 
